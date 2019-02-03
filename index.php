6177bb3269a141491e5c33a43995006188f37575
zak cnaan <zak@zakcnaan.com>
2019-02-03 08:53:47 +0200

    pagination

--- index.php
+++ index.php
@@ -11,6 +11,6 @@ $context = Timber::get_context();
 $context['posts'] = Timber::get_posts();
 $templates = array( 'pages/index.twig' );
 if ( is_home() ) {
-	array_unshift( $templates, 'pages/home.twig' );
+	array_unshift( $templates, 'pages/blog-page.twig' );
 }
 Timber::render( $templates, $context );
